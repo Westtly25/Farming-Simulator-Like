b1d294f892e0e6199a7cf100f089d03e959ba31d
westtly25 <westtly25@gmail.com>
2022-09-27 09:44:41 +0300

    Code optimizations, updated Inventory manager

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -7,36 +7,37 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
 
     private Dictionary<int, ItemDetails> itemDetailsDictionary;
 
-    private int[] selectedInventoryItem; // the index of the array is the inventory list, and the value is the item code
+    private int[] selectedInventoryItem;
 
     public List<InventoryItem>[] inventoryLists;
 
-    [HideInInspector] public int[] inventoryListCapacityIntArray; // the index of the array is the inventory list (from the InventoryLocation enum), and the value is the capacity of that inventory list
+    [HideInInspector] public int[] inventoryListCapacityIntArray;
 
     [SerializeField] private SO_ItemList itemList = null;
 
-    private string _iSaveableUniqueID;
+    private string iSaveableUniqueID;
     public string ISaveableUniqueID
     {
-        get => _iSaveableUniqueID;
-        set => _iSaveableUniqueID = value;
+        get => iSaveableUniqueID;
+        set => iSaveableUniqueID = value;
     }
 
-    private GameObjectSave _gameObjectSave;
-    public GameObjectSave GameObjectSave { get { return _gameObjectSave; } set { _gameObjectSave = value; } }
+    private GameObjectSave gameObjectSave;
+    public GameObjectSave GameObjectSave
+    {
+        get => gameObjectSave;
+        set => gameObjectSave = value;
+    }
 
 
     protected override void Awake()
     {
         base.Awake();
 
-        // Create Inventory Lists
         CreateInventoryLists();
 
-        // Create item details dictionary
         CreateItemDetailsDictionary();
 
-        // Initailise selected inventory item array
         selectedInventoryItem = new int[(int)InventoryLocation.count];
 
         for (int i = 0; i < selectedInventoryItem.Length; i++)
@@ -44,7 +45,6 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
             selectedInventoryItem[i] = -1;
         }
 
-        // Get unique ID for gameobject and create save data object
         ISaveableUniqueID = GetComponent<GenerateGUID>().GUID;
 
         GameObjectSave = new GameObjectSave();
@@ -75,16 +75,11 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
             inventoryLists[i] = new List<InventoryItem>();
         }
 
-        // initialise inventory list capacity array
         inventoryListCapacityIntArray = new int[(int)InventoryLocation.count];
 
-        // initialise player inventory list capacity
         inventoryListCapacityIntArray[(int)InventoryLocation.player] = Settings.playerInitialInventoryCapacity;
     }
 
-    /// <summary>
-    ///  Populates the itemDetailsDictionary from the scriptable object items list 
-    /// </summary>
     private void CreateItemDetailsDictionary()
     {
         itemDetailsDictionary = new Dictionary<int, ItemDetails>();
@@ -94,10 +89,7 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
             itemDetailsDictionary.Add(itemDetails.itemCode, itemDetails);
         }
     }
-
-    /// <summary>
-    /// Add an item to the inventory list for the inventoryLocation and then destroy the gameObjectToDelete
-    /// </summary>
+   
     public void AddItem(InventoryLocation inventoryLocation, Item item, GameObject gameObjectToDelete)
     {
         AddItem(inventoryLocation, item);
@@ -105,15 +97,11 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
         Destroy(gameObjectToDelete);
     }
 
-    /// <summary>
-    /// Add an item to the inventory list for the inventoryLocation
-    /// </summary>
     public void AddItem(InventoryLocation inventoryLocation, Item item)
     {
         int itemCode = item.ItemCode;
         List<InventoryItem> inventoryList = inventoryLists[(int)inventoryLocation];
 
-        // Check if inventory already contains the item
         int itemPosition = FindItemInInventory(inventoryLocation, itemCode);
 
         if (itemPosition != -1)
@@ -125,18 +113,13 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
             AddItemAtPosition(inventoryList, itemCode);
         }
 
-        //  Send event that inventory has been updated
         EventHandler.CallInventoryUpdatedEvent(inventoryLocation, inventoryLists[(int)inventoryLocation]);
     }
 
-    /// <summary>
-    /// Add an item of type itemCode to the inventory list for the inventoryLocation
-    /// </summary>
     public void AddItem(InventoryLocation inventoryLocation, int itemCode)
     {
         List<InventoryItem> inventoryList = inventoryLists[(int)inventoryLocation];
 
-        // Check if inventory already contains the item
         int itemPosition = FindItemInInventory(inventoryLocation, itemCode);
 
         if (itemPosition != -1)
@@ -148,16 +131,9 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
             AddItemAtPosition(inventoryList, itemCode);
         }
 
-        //  Send event that inventory has been updated
         EventHandler.CallInventoryUpdatedEvent(inventoryLocation, inventoryLists[(int)inventoryLocation]);
     }
 
-
-
-
-    /// <summary>
-    /// Add item to the end of the inventory
-    /// </summary>
     private void AddItemAtPosition(List<InventoryItem> inventoryList, int itemCode)
     {
         InventoryItem inventoryItem = new InventoryItem();
@@ -165,13 +141,8 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
         inventoryItem.itemCode = itemCode;
         inventoryItem.itemQuantity = 1;
         inventoryList.Add(inventoryItem);
-
-        //DebugPrintInventoryList(inventoryList);
     }
 
-    /// <summary>
-    /// Add item to position in the inventory
-    /// </summary>
     private void AddItemAtPosition(List<InventoryItem> inventoryList, int itemCode, int position)
     {
         InventoryItem inventoryItem = new InventoryItem();
@@ -180,18 +151,10 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
         inventoryItem.itemQuantity = quantity;
         inventoryItem.itemCode = itemCode;
         inventoryList[position] = inventoryItem;
-
-        
-        //DebugPrintInventoryList(inventoryList);
     }
-
-    ///<summary>
-    ///Swap item at fromItem index with item at toItem index in inventoryLocation inventory list
-    ///</summary>
 
     public void SwapInventoryItems(InventoryLocation inventoryLocation, int fromItem, int toItem)
     {
-        // if fromItem index and toItemIndex are within the bounds of the list, not the same, and greater than or equal to zero
         if (fromItem < inventoryLists[(int)inventoryLocation].Count && toItem < inventoryLists[(int)inventoryLocation].Count
              && fromItem != toItem && fromItem >= 0 && toItem >= 0)
         {
@@ -201,25 +164,15 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
             inventoryLists[(int)inventoryLocation][toItem] = fromInventoryItem;
             inventoryLists[(int)inventoryLocation][fromItem] = toInventoryItem;
 
-            //  Send event that inventory has been updated
             EventHandler.CallInventoryUpdatedEvent(inventoryLocation, inventoryLists[(int)inventoryLocation]);
         }
     }
 
-    /// <summary>
-    /// Clear the selected inventory item for inventoryLocation
-    /// </summary>
     public void ClearSelectedInventoryItem(InventoryLocation inventoryLocation)
     {
         selectedInventoryItem[(int)inventoryLocation] = -1;
     }
 
-
-
-    /// <summary>
-    /// Find if an itemCode is already in the inventory. Returns the item position
-    /// in the inventory list, or -1 if the item is not in the inventory
-    /// </summary>
     public int FindItemInInventory(InventoryLocation inventoryLocation, int itemCode)
     {
         List<InventoryItem> inventoryList = inventoryLists[(int)inventoryLocation];
@@ -235,10 +188,6 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
         return -1;
     }
 
-    /// <summary>
-    /// Returns the itemDetails (from the SO_ItemList) for the itemCode, or null if the item code doesn't exist
-    /// </summary>
-
     public ItemDetails GetItemDetails(int itemCode)
     {
         ItemDetails itemDetails;
@@ -253,9 +202,6 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
         }
     }
 
-    /// <summary>
-    /// Returns the itemDetails (from the SO_ItemList) for the currently selected item in the inventoryLocation , or null if an item isn't selected
-    /// </summary>
     public ItemDetails GetSelectedInventoryItemDetails(InventoryLocation inventoryLocation)
     {
         int itemCode = GetSelectedInventoryItem(inventoryLocation);
@@ -271,19 +217,10 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
     }
 
 
-    /// <summary>
-    /// Get the selected item for inventoryLocation - returns itemCode or -1 if nothing is selected
-    /// </summary>
     private int GetSelectedInventoryItem(InventoryLocation inventoryLocation)
     {
         return selectedInventoryItem[(int)inventoryLocation];
     }
-
-
-
-    /// <summary>
-    /// Get the item type description for an item type - returns the item type description as a string for a given ItemType
-    /// </summary>
 
     public string GetItemTypeDescription(ItemType itemType)
     {
@@ -337,13 +274,10 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
         // Create new scene save
         SceneSave sceneSave = new SceneSave();
 
-        // Remove any existing scene save for persistent scene for this gameobject
         GameObjectSave.sceneData.Remove(Settings.PersistentScene);
 
-        // Add inventory lists array to persistent scene save
         sceneSave.listInvItemArray = inventoryLists;
 
-        // Add  inventory list capacity array to persistent scene save
         sceneSave.intArrayDictionary = new Dictionary<string, int[]>();
         sceneSave.intArrayDictionary.Add("inventoryListCapacityArray", inventoryListCapacityIntArray);
 
@@ -360,28 +294,22 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
         {
             GameObjectSave = gameObjectSave;
 
-            // Need to find inventory lists - start by trying to locate saveScene for game object
             if (gameObjectSave.sceneData.TryGetValue(Settings.PersistentScene, out SceneSave sceneSave))
             {
-                // list inv items array exists for persistent scene
                 if (sceneSave.listInvItemArray != null)
                 {
                     inventoryLists = sceneSave.listInvItemArray;
 
-                    //  Send events that inventory has been updated
                     for (int i = 0; i < (int)InventoryLocation.count; i++)
                     {
                         EventHandler.CallInventoryUpdatedEvent((InventoryLocation)i, inventoryLists[i]);
                     }
 
-                    // Clear any items player was carrying
                     Player.Instance.ClearCarriedItem();
 
-                    // Clear any highlights on inventory bar
                     inventoryBar.ClearHighlightOnInventorySlots();
                 }
 
-                // int array dictionary exists for scene
                 if (sceneSave.intArrayDictionary != null && sceneSave.intArrayDictionary.TryGetValue("inventoryListCapacityArray", out int[] inventoryCapacityArray))
                 {
                     inventoryListCapacityIntArray = inventoryCapacityArray;
@@ -396,22 +324,16 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
         // Nothing required her since the inventory manager is on a persistent scene;
     }
 
-        public void ISaveableRestoreScene(string sceneName)
+    public void ISaveableRestoreScene(string sceneName)
     {
         // Nothing required here since the inventory manager is on a persistent scene;
     }
 
-
-
-
-    /// <summary>
-    /// Remove an item from the inventory, and create a game object at the position it was dropped
-    /// </summary>
+   
     public void RemoveItem(InventoryLocation inventoryLocation, int itemCode)
     {
         List<InventoryItem> inventoryList = inventoryLists[(int)inventoryLocation];
 
-        // Check if inventory already contains the item
         int itemPosition = FindItemInInventory(inventoryLocation, itemCode);
 
         if (itemPosition != -1)
@@ -419,7 +341,6 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
             RemoveItemAtPosition(inventoryList, itemCode, itemPosition);
         }
 
-        //  Send event that inventory has been updated
         EventHandler.CallInventoryUpdatedEvent(inventoryLocation, inventoryLists[(int)inventoryLocation]);
 
     }
@@ -442,22 +363,8 @@ public class InventoryManager :SingletonMonobehaviour<InventoryManager>, ISaveab
         }
     }
 
-    /// <summary>
-    /// Set the selected inventory item for inventoryLocation to itemCode
-    /// </summary>
     public void SetSelectedInventoryItem(InventoryLocation inventoryLocation, int itemCode)
     {
         selectedInventoryItem[(int)inventoryLocation] = itemCode;
     }
-
-
-    //private void DebugPrintInventoryList(List<InventoryItem> inventoryList)
-    //{
-    //    foreach (InventoryItem inventoryItem in inventoryList)
-    //    {
-    //        Debug.Log("Item Description:" + InventoryManager.Instance.GetItemDetails(inventoryItem.itemCode).itemDescription + "    Item Quantity: " + inventoryItem.itemQuantity);
-    //    }
-    //    Debug.Log("******************************************************************************");
-    //}
-
 }
